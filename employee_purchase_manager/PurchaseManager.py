9c1c9102d24d3299022edd00cba5156daf410fd6
Harlan Ferguson <harlanjazz@hotmail.com>
2023-02-06 11:44:40 -0500

    added classes added classes for purchases

---
+++ employee_purchase_manager/PurchaseManager.py
@@ -0,0 +1,14 @@
+#Decided to do a purchase Id for search functionality.
+
+from Purchase import Purchase
+
+class PurchaseManager:
+    def __init__(self):
+        self.item_list = []
+        self.purchase_id = 3000
+    
+    #def add_purchase(self,Purchase):
+        #add to employee class purchase total and discount total
+        #grab the employee discount, do that math to the item price
+        #then store that to employee total, and the amount discounted to employee discount
+
